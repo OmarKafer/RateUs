cb828bece77f1aa3b577c89ddda4bce6fdcab719
Oscar <oscarvillalba939@gmail.com>
2019-02-11 19:36:20 +0100

    Modificación interfaz login

---
+++ app/src/main/java/pi/rateusteam/rateus/Fragments/VotacionFragment.java
@@ -0,0 +1,65 @@
+package pi.rateusteam.rateus.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pi.rateusteam.rateus.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link VotacionFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link VotacionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VotacionFragment extends Fragment {
+
+
+
+    public VotacionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VotacionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VotacionFragment newInstance(String param1, String param2) {
+        VotacionFragment fragment = new VotacionFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_votacion, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+    
+}
