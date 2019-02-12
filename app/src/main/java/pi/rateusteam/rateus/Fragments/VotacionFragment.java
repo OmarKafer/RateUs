46343920dc42ef20532dca1b16d71ba69475a7c0
Casa\Omar Kafernawi <omarku5@hotmail.com>
2019-02-12 16:46:38 +0100

    Cambios en Lector

--- app/src/main/java/pi/rateusteam/rateus/Fragments/VotacionFragment.java
+++ app/src/main/java/pi/rateusteam/rateus/Fragments/VotacionFragment.java
@@ -10,17 +10,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.KeyEvent;
+
+import com.google.android.gms.vision.text.Line;
 
 import pi.rateusteam.rateus.Controladores.LectorActivity;
 import pi.rateusteam.rateus.R;
 
+import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
+import static pi.rateusteam.rateus.MainActivity.permitirAtras;
 
 public class VotacionFragment extends Fragment implements View.OnClickListener{
 
     private static final int ACTIVITY_LECTOR = 2;
     private LinearLayout lEscanear;
+    private LinearLayout lVotos;
+    private LinearLayout lBoton;
 
     public VotacionFragment() {
         // Required empty public constructor
@@ -42,6 +48,15 @@ public class VotacionFragment extends Fragment implements View.OnClickListener{
         View v = inflater.inflate(R.layout.fragment_votacion, container, false);
         lEscanear = v.findViewById(R.id.lEscanear);
         lEscanear.setOnClickListener(this);
+
+        lVotos = v.findViewById(R.id.lVotos);
+        lVotos.setOnClickListener(this);
+
+        lBoton =v.findViewById(R.id.lBoton);
+        lBoton.setOnClickListener(this);
+
+        ventanaSinVotos();
+
         return v;
     }
 
@@ -62,6 +77,10 @@ public class VotacionFragment extends Fragment implements View.OnClickListener{
                 if(resultCode == RESULT_OK) {
                     String token = data.getStringExtra("token");
                     Log.d("Omar", "El token es: " + token);
+                    ventanaConVotos();
+                } else if(resultCode == RESULT_CANCELED) {
+                    Log.d("Omar", "Se ha cancelado el activity");
+                    ventanaSinVotos();
                 }
                 break;
         }
@@ -80,4 +99,19 @@ public class VotacionFragment extends Fragment implements View.OnClickListener{
         Intent i = new Intent(getContext(), LectorActivity.class);
         startActivityForResult(i, ACTIVITY_LECTOR);
     }
+
+    public void ventanaSinVotos() {
+        permitirAtras = true;
+        lEscanear.setVisibility(View.VISIBLE);
+        lVotos.setVisibility(View.GONE);
+        lBoton.setVisibility(View.GONE);
+    }
+
+    public void ventanaConVotos() {
+        permitirAtras = false;
+        lEscanear.setVisibility(View.GONE);
+        lVotos.setVisibility(View.VISIBLE);
+        lBoton.setVisibility(View.VISIBLE);
+    }
+
 }
--- app/src/main/java/pi/rateusteam/rateus/MainActivity.java
+++ app/src/main/java/pi/rateusteam/rateus/MainActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 
 import pi.rateusteam.rateus.Controladores.GestorFirebase;
 import pi.rateusteam.rateus.Fragments.LoginFragment;
+import pi.rateusteam.rateus.Fragments.VotacionFragment;
 import pi.rateusteam.rateus.Interfaces.NavigationHost;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost {
 
     private GestorFirebase gestorFirebase;
+    public static boolean permitirAtras = true;
+    private VotacionFragment votacionFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
 
     @Override
     public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        permitirAtras = true;
+        if(fragment instanceof VotacionFragment) {
+            this.votacionFragment = (VotacionFragment) fragment;
+        }
         FragmentTransaction transaction =
                 getSupportFragmentManager()
                         .beginTransaction()
@@ -48,6 +57,19 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            if(permitirAtras) {
+                return super.onKeyDown(keyCode, event);
+            } else {
+                navigateTo(new VotacionFragment(), false);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
